cfc6edcc9f3335c8ababe3baadbd96ee91180b86
zyrouge <naveencnaveen96@gmail.com>
2020-06-14 13:28:23 +0530

    Fixes

--- src/commands/bots.js
+++ src/commands/bots.js
@@ -13,50 +13,50 @@ module.exports = {
                 embed: {
                     author: {
                         name: `${user.tag}'s Information`,
-                        icon_url: user.avatarURl()
+                        icon_url: user.avatarURL()
                     },
                     description: `${botDB.dataValues.shortDesc}`,
                     fields: [
                         {
                             name: `Prefix`,
-                            value: `${bot.dataValues.prefix}`,
+                            value: `${botDB.dataValues.prefix}`,
                             inline: true
                         },
                         {
                             name: `Owner`,
-                            value: `<@${bot.dataValues.ownerID}>`,
+                            value: `<@${botDB.dataValues.ownerID}>`,
                             inline: true
                         },
                         {
                             name: `Links`,
                             value: [
-                                `${bot.dataValues.botWebsite ? `**Website:** ${bot.dataValues.botWebsite}`: ""}`,
-                                `${bot.dataValues.botServer ? `**Support Server:** https://discord.gg/${bot.dataValues.botServer}`: ""}`,
-                                `**Invite:** ${bot.dataValues.customInvite || `https://discordapp.com/oauth2/authorize?client_id=${user.id}&scope=bot&permissions=0`}`
+                                `${botDB.dataValues.botWebsite ? `**Website:** ${botDB.dataValues.botWebsite}`: ""}`,
+                                `${botDB.dataValues.botServer ? `**Support Server:** https://discord.gg/${botDB.dataValues.botServer}`: ""}`,
+                                `**Invite:** ${botDB.dataValues.customInvite || `https://discordapp.com/oauth2/authorize?client_id=${user.id}&scope=bot&permissions=0`}`
                             ].filter(x => x).join("\n"),
                             inline: true
                         },
                         {
                             name: `Tags`,
-                            value: `${bot.dataValues.botTags.join(" ,")}`,
+                            value: `${botDB.dataValues.botTags.join(" ,")}`,
                             inline: true
                         },
                         {
                             name: `Upvotes`,
-                            value: `**Today's Upvotes:** ${dailyUpvotes}\n**Total Upvotes:** ${bot.dataValues.totalUpvotes}`,
+                            value: `**Today's Upvotes:** ${dailyUpvotes}\n**Total Upvotes:** ${botDB.dataValues.totalUpvotes}`,
                             inline: true
                         },
                         {
                             name: `Guilds`,
-                            value: `${bot.dataValues.guildsCount} Servers`,
+                            value: `${botDB.dataValues.guildsCount} Servers`,
                             inline: true
                         },
                         {
                             name: `Others`,
                             value: [
-                                `**Submitted on:** ${new Date(bot.dataValues.submittedOn).toLocaleString()}`,
-                                `**Approved on:** ${new Date(bot.dataValues.approvedOn).toLocaleString()}`,
-                                `**Tested by:** <@${bot.dataValues.testedBy}>`
+                                `**Submitted on:** ${new Date(botDB.dataValues.submittedOn).toLocaleString()}`,
+                                `**Approved on:** ${new Date(botDB.dataValues.approvedOn).toLocaleString()}`,
+                                `**Tested by:** <@${botDB.dataValues.testedBy}>`
                             ].join("\n"),
                             inline: true
                         },
@@ -74,7 +74,7 @@ module.exports = {
                 embed: {
                     author: {
                         name: `${user.tag}'s Bots`,
-                        icon_url: user.avatarURl()
+                        icon_url: user.avatarURL()
                     },
                     description: `${botDB.map(bot => `<@${bot.dataValues.botID}>`).join("\n")}`,
                     timestamp: new Date(),
